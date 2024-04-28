db895cc26ff21875e78b84b3b5823929f77cd238
essaouidiyassine670 <167012904+essaouidiyassine670@users.noreply.github.com>
2024-04-28 23:34:25 +0100

    Create tomp4.js

---
+++ plugins/tomp4.js
@@ -0,0 +1,17 @@
+import { webp2mp4 } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || q.mediaType || ''
+	if (/webp/.test(mime) || /ptv/.test(q.mtype) || q.isAnimated) {
+	let out = await q.download()
+	if (/webp/.test(mime)) out = await webp2mp4(out).catch(_ => null) || Buffer.alloc(0)
+	await conn.sendFile(m.chat, out, '', '\n متابعة لصاحب البوت هنا \n *_www.instagram.com/essaouidi_yassine_*', m)
+	} else throw `قم بالرد على الملصق لتحويله إلى فيديو *${usedPrefix + command}*`
+}
+
+handler.help = ['tovid']
+handler.tags = ['sticker']
+handler.command = /^((tovid))$/i
+
+export default handler
