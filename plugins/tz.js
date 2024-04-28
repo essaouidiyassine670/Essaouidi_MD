f0fe83f087fa032e990b4500d7104bdd5c3bad43
essaouidiyassine670 <167012904+essaouidiyassine670@users.noreply.github.com>
2024-04-28 19:31:32 +0100

    Update tz.js

--- plugins/tz.js
+++ plugins/tz.js
@@ -9,7 +9,7 @@ const handler = async (m, {conn}) => {
 
 ▢ morocco     : ${tzAF}
   ${String.fromCharCode(8206).repeat(850)}
-  ▢ instagram.com/noureddine_ouafy`}, {quoted: m});
+  ▢ instagram.com/essaouidi_yassine`}, {quoted: m});
   };
 handler.help = ["tz"]
 handler.tags = ["infobot"]
