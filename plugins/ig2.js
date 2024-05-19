6c3bdbad0cb0cbfecd1f995baabd3c953d199744
Essaouidi  Yassine <167012904+essaouidiyassine670@users.noreply.github.com>
2024-05-19 18:17:25 +0100

    Update ig2.js

--- plugins/ig2.js
+++ plugins/ig2.js
@@ -1,39 +1,79 @@
-import axios from 'axios'
+import fetch from 'node-fetch';
+import axios from 'axios';
+import instagramGetUrl from 'instagram-url-direct';
+import {instagram} from '@xct007/frieren-scraper';
+import {instagramdl} from '@bochilteam/scraper';
+import instagramDl from '@sasmeee/igdl';
+import {fileTypeFromBuffer} from 'file-type';
 
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    return conn.reply(m.chat, 'يرجى إدخال رابط إنستجرام بعد الأمر. \n .instagram2 https://www.instagram.com/reel/C50Bt8vIKNi/?igsh=bDN3eTgwbTBkY29w', m)
-  }
 
-  let url = `https://vihangayt.me/download/instagram?url=${encodeURIComponent(text)}`
-  
+
+const handler = async (m, {conn, args, command, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.descargas_instagram
+
+
+  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link_`;
+  m.reply(global.wait);
   try {
-    // جلب مقطع الفيديو من إنستجرام باستخدام Axios
-    const response = await axios.get(url)
-    if (!response.data.status) {
-      throw new Error(`حدث خطأ في جلب البيانات من ${url}`)
-    }
-
-    const data = response.data.data
-    if (data && data.data && data.data.length > 0) {
-      const videoURL = data.data[0].url
-      const caption = data.data[0].type
-
-      // إرسال الملف مع وضع نوعه كتسمية
-      await conn.sendFile(m.chat, videoURL, 'instagram_reel.mp4', '*الفيديو الخاص بك*\n _*instagram.com/essaouidi_yassine*_, m)
-    } else {
-      conn.reply(m.chat, 'تعذر العثور على مقطع فيديو من إنستجرام.', m)
-    }
-  } catch (error) {
-    console.error(error)
-    conn.reply(m.chat, 'حدث خطأ أثناء جلب مقطع الفيديو من إنستجرام.', m)
-  }
+const img = await instagramDl(args[0]);
+for (let i = 0; i < img.length; i++) {
+    const bufferInfo = await getBuffer(img[i].download_link);
+        if (bufferInfo.detectedType.mime.startsWith('image/')) {
+            await conn.sendMessage(m.chat, {image: {url: img[i].download_link}}, {quoted: m});
+        } else if (bufferInfo.detectedType.mime.startsWith('video/')) {
+            await conn.sendMessage(m.chat, {video: {url: img[i].download_link }}, {quoted: m});
+        }
 }
+  } catch {   
+  try {
+    const datTa = await instagram.download(args[0]);
+    for (const urRRl of datTa) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+      const tXXxt = `${tradutor.texto2} _${shortUrRRl}_`.trim();
+      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+  } catch {
+      try {
+        const resultss = await instagramGetUrl(args[0]).url_list[0];
+        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+        const txt2 = `${tradutor.texto2} _${shortUrl2}_`.trim();
+        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
+      } catch {
+        try {
+          const resultssss = await instagramdl(args[0]);
+          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+          const txt4 = `${tradutor.texto2} _${shortUrl3}_`.trim();
+          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
+        } catch {
+          try {
+            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
+            const json = await human.json();
+            const videoig = json.result;
+            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+            const txt1 = `${tradutor.texto2} _${shortUrl1}_`.trim();
+            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
+          } catch {
+            throw `${tradutor.texto3}`;
+          }
+        }
+      }
+    }
+  }
+};
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+export default handler;
 
-handler.command = /^(igdl2|ig2|instagram2)$/i
-handler.tags = ['downloader']
-handler.help = ['instagram2'']
-handler.premium = false
-handler.limit = true
-
-export default handler
+const getBuffer = async (url, options) => {
+    options = options || {};
+    const res = await axios({method: 'get', url, headers: {'DNT': 1, 'Upgrade-Insecure-Request': 1}, ...options, responseType: 'arraybuffer'});
+    const buffer = Buffer.from(res.data, 'binary');
+    const detectedType = await fileTypeFromBuffer(buffer);
+    if (!detectedType || (detectedType.mime !== 'image/jpeg' && detectedType.mime !== 'image/png' && detectedType.mime !== 'video/mp4')) {
+        return null;
+    }
+    return { buffer, detectedType };
+};
